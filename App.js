d6997115384c56f869fe0b8a51250fd11f2a8c86
HPRP01 <55462701+HPRP01@users.noreply.github.com>
2021-02-27 11:59:16 -0500

    Touchable

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  TouchableHighlight, 
+  View, 
+  Image, 
+  SafeAreaView 
+} from 'react-native';
 
 export default function App() {
+  
+  const handlePress = () => console.log("Test pressed");
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Hello
+      </Text>
+      <TouchableHighlight onPress={() => console.log("ImagePressed")}>
+        <Image 
+          source={{ 
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300"}}
+        />
+      </TouchableHighlight>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
+
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgb(197, 242, 250)',
     alignItems: 'center',
     justifyContent: 'center',
+    
+  },
+
+  stretch: {
+    //width: 320,
+    height: 200,
+    resizeMode: 'contain',
   },
 });
