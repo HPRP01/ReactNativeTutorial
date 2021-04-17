13a3562b29f71da327d8cbb2caa0018fb4b8ee63
HPRP01 <55462701+HPRP01@users.noreply.github.com>
2021-04-17 15:50:21 -0400

    time to do real homework

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
 //import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator} from '@react-navigation/stack';
+
 import React from 'react';
 import { 
   View,
@@ -11,10 +14,12 @@ import OpeningScreen from './app/screens/OpeningScreen';
 import ScrollSCreen from './app/screens/ScrollSCreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import { startClock } from 'react-native-reanimated';
+
 
 //const backgroundImage = <Image source={require('./assets/background.jpg')}/>;
 //UPDATE
 export default function App() {
   return <ScrollSCreen/>;
-
+ 
 }
